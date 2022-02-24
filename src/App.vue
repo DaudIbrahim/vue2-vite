9f63deda98a28483402a413b324cf6f8d7b2ccd2
Mohammad Daud Ibrahim <iamdaud18@gmail.com>
2022-02-24 18:51:47 +0600

    Counter Functional

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+    <Counter/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Counter from './components/Counter.vue';
+export default {
+  components: {
+    HelloWorld,
+    Counter
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Counter Component: {{ number }}</h1>
+    <div>
+      <input type="number" v-model="number" />
+      <button v-on:click="increment">Increment</button>
+      <button v-on:click="decrement">Decrement</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/composition-api";
+const number = ref(0);
+
+function increment() {
+  number.value++;
+}
+
+function decrement() {
+  number.value--;
+}
+</script>
+
+<style scoped>
+</style>
